b26dbe9694a521c6365b5d3e2cd3d8259282da3e
viktormokrecov <viktormokrecov@gmail.com>
2020-03-24 12:07:14 +0300

    add security

--- src/main/java/com/vmokrecov/helloservice/controllers/HelloController.java
+++ src/main/java/com/vmokrecov/helloservice/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.vmokrecov.helloservice.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -12,8 +14,15 @@ public class HelloController {
         return Mono.just("{ \"message\": \"Home\" }");
     }
 
-    @GetMapping("hello")
-    public Mono<String> hello() {
+    @GetMapping("/hello")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Mono<String> getHello() {
         return Mono.just("{ \"message\": \"Hello\" }");
+    }
+
+    @PostMapping("/hello")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public Mono<String> postHello() {
+        return Mono.just("{ \"message\": \"Hello Post\" }");
     }
 }
