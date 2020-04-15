a7865d2e4a5efdff802dad52be3ea5baaa1cb022
viktormokrecov <viktormokrecov@gmail.com>
2020-04-15 14:37:17 +0300

    add mock service for test

--- src/test/java/com/vmokrecov/helloservice/controllers/HelloControllerTest.java
+++ src/test/java/com/vmokrecov/helloservice/controllers/HelloControllerTest.java
@@ -1,14 +1,19 @@
 package com.vmokrecov.helloservice.controllers;
 
+import com.vmokrecov.helloservice.services.MessagesService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @WebFluxTest
 class HelloControllerTest {
+
+    @MockBean
+    private MessagesService service;
 
     @Autowired
     private WebTestClient client;
